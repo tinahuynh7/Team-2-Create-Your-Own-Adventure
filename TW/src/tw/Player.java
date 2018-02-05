8a34a76597bea3aa1d0ba20b818028af3401ae14
kyllief <kylliefurukawa@sccstudents.org>
2018-02-05 12:31:34 -0700

    Test Commit

---
+++ TW/src/tw/Player.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw;
+import java.util.ArrayList; 
+/**
+ *
+ * @author kylliefurukawa
+ */
+public class Player {
+    
+    int position = 0; 
+    int health;
+    ArrayList <Item> inventory; 
+    
+    Player(){
+        
+    }
+    
+    
+}
