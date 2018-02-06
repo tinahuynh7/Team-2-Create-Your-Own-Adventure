6379f279eadc33b85215463a683ac95f0f0128cf
SevereHayDHD <haydenhickman@sccstudents.org>
2018-02-06 14:58:24 -0700

    Added Monster And Tile

---
+++ Tile.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author haydenhickman
+ */
+public class Tile {
+   boolean FTE = true;
+   boolean Room;
+   Monster monster;
+   Player player;
+   Item item;
+}
