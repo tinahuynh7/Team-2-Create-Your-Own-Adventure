f70858666366c716383e53a8a53434c45d1c77b9
kyllief <kylliefurukawa@sccstudents.org>
2018-02-09 12:51:10 -0700

    Changes to Menu and Player

--- Menu.java
+++ Menu.java
@@ -10,13 +10,22 @@ import java.util.Scanner;
 */
 public class Menu {
     Scanner sc = new Scanner(System.in); 
+    Player new_player; 
     
-    Menu(){
+    Menu(Player new_player){
+        this.new_player = new_player; 
     }
     
-    public String input_selection() { 
+    public String input_command() { 
     
-        String input_selection = sc.nextLine();
+        String input_command = sc.nextLine();
+        
+        return input_command; 
+    }
+    
+    public int input_selection() {
+        
+        int input_selection = sc.nextInt();
         
         return input_selection; 
     }
@@ -24,7 +33,7 @@ public class Menu {
     public void main_menu() {
         System.out.println("What would you like to do? (Type 'HELP' for a list of commands)"); 
         
-        String selection = input_selection(); 
+        String selection = input_command(); 
         
         if (selection.equalsIgnoreCase("HELP")) {
             System.out.println("to view inventory: 'INVENTORY'");
@@ -32,23 +41,84 @@ public class Menu {
             main_menu(); 
         }
         
-        if (selection.equalsIgnoreCase("INVENTORY")) {
-            System.out.println("test inventory ");
+        else if (selection.equalsIgnoreCase("INVENTORY")) {
+            System.out.println("Here is your inventory: "); 
+            System.out.println(new_player.toString()); 
+            inventory();
         }
         
-        if (selection.equalsIgnoreCase("MOVE")) {
+        else if (selection.equalsIgnoreCase("MOVE")) {
             //move function 
+        }
+        
+        else {
+            System.out.println ("That is not a valid command."); 
+            main_menu(); 
         }
 
     }
+    
+    public void move_menu() {
+        
+        System.out.println("Would you like to move 'FOWARD', 'BACKWARDS', or 'ENTER ROOM'?"); 
+        
+        String selection = input_command(); 
+        
+        if (selection.equalsIgnoreCase("FOWARD")) {
+            //player position++
+        }
+        
+        if (selection.equalsIgnoreCase("BACKWARDS")) {
+            //player position--           
+        }
+        
+        if (selection.equalsIgnoreCase("ENTER ROOM")) {
+            //check if room
+            //call room 
+        }
+    }
     public void weapon_selection() {
         
-        System.out.println("Select your weapon" );
+        System.out.println("Select your primary weapon:" );
         
+        System.out.println(new_player.toString()); 
+        int selection = input_selection(); 
         
-        String selection = input_selection(); 
+        if (selection == 1) {
+            System.out.println("you selected " + new_player.inventory.get(0).name() + ".");
+        }        
+    }
+    
+    public void inventory() {
+        System.out.println("Would you like to 'DROP ITEM' or 'USE POTION'?");
         
+        String selection = input_command(); 
+        if (selection.equalsIgnoreCase("DROP ITEM")) {
+            drop_item_menu();          
+        }
         
+        if (selection.equalsIgnoreCase("USE POTION")) {
+            System.out.println("used potion");
+            //for future
+        }
+    }
+    
+    public void drop_item_menu() {
+        
+        System.out.println("Which item would you like to drop?");
+        new_player.toString(); 
+            
+        int selection = input_selection(); 
+        
+        if (selection == 1) {
+            new_player.remove_item(1);
+        }
+        //use switch for this 
+        
+        System.out.println("Here is your new inventory: ");
+        new_player.toString(); 
+    
+             
     }
     
     
