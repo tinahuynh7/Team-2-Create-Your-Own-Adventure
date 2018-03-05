d41c79fd5f68bef5346ea1b838db35f418120b77
SevereHayDHD <haydenhickman@sccstudents.org>
2018-03-05 09:00:57 -0700

    monsters
    Here they are

---
+++ Goblin.java
@@ -0,0 +1,9 @@
+
+public class Goblin extends Monster {
+  
+
+    Goblin(){
+    this.health = 100;
+    this.name = "Goblin";
+    }
+}
---
+++ Slime.java
@@ -0,0 +1,9 @@
+
+public class Slime extends Monster {
+  
+
+    Slime(){
+    this.health = 100;
+    this.name = "Slime";
+    }
+}
